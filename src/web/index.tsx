7c1ad3ef2e96bcd2605007d266cb40d8a704160e
stephenyu <stephen@canva.com>
2020-05-16 10:31:09 +1000

    More

--- src/web/index.tsx
+++ src/web/index.tsx
@@ -1,23 +1,43 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
 import { useRecoilState, RecoilRoot } from 'recoil';
 
-import { Task } from './task/task';
-import { InputTask } from './input_task/input_task';
-import { TaskState } from './atoms/task.atom';
+import { TasksState } from './atoms/tasks.atom';
 
-const CurrentTask = () => {
-  const [task] = useRecoilState(TaskState);
-  return <span>Current Task is {task}</span>
+import { ListTasks } from './list_tasks/list_tasks';
+import { InputTask } from './input_task/input_task';
+
+const LocationHandler = (): JSX.Element => {
+  const [tasks, setTasks] = useRecoilState(TasksState);
+
+
+  React.useEffect(() => {
+    const hash = decodeURI(location.hash);
+    if (hash) {
+      const match = hash.match(/#(.*)/);
+      const tasks = match[1].split(',');
+      setTasks(tasks);
+    }
+  }, []);
+
+
+  React.useEffect(() => {
+    if (tasks.length > 0) {
+      const urlData = tasks.join(',');
+      const url = location.origin + location.pathname + `#${urlData}`;
+      location.href = encodeURI(url);
+    }
+  }, [tasks]);
+
+  return null;
 }
 
 const App = () => {
-  const
   return <div>
     <RecoilRoot>
-      <InputTask onSubmit={() => void 0}/>
-      <CurrentTask />
+      <LocationHandler/>
+      <ListTasks/>
+      <InputTask/>
     </RecoilRoot>
   </div>
 }
--- src/web/input_task/input_task.tsx
+++ src/web/input_task/input_task.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
+import { useRecoilState } from 'recoil';
+
+import { TasksState } from '../atoms/tasks.atom';
 
 interface InputTaskProps {
-    onSubmit: (task: string) => void;
+  onSubmit: (task: string) => void;
 }
 
-export const InputTask = ({onSubmit}: InputTaskProps) => {
+export const InnerInputTask = ({onSubmit}: InputTaskProps) => {
   const [value, setValue] = React.useState<string>('');
 
   const onChange = React.useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
@@ -12,10 +15,29 @@ export const InputTask = ({onSubmit}: InputTaskProps) => {
     setValue(value);
   }, []);
 
-  const onSave = React.useCallback(() => onSubmit(value), []);
+  const onSave = React.useCallback(() => {
+    onSubmit(value);
+    setValue('');
+  }, [value]);
+
+  const onKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.keyCode === 13) {
+      onSave();
+    }
+  }
 
   return <>
-    <input type="text" value={value} onChange={onChange}/>
+    <input type="text" value={value} onChange={onChange} onKeyDown={onKeyDown}/>
     <button onClick={onSave}>Save</button>
   </>
+}
+
+export const InputTask = () => {
+  const [tasks, setTasks] = useRecoilState(TasksState);
+
+  const onSubmit = (task: string) => {
+    setTasks([...tasks, task]);
+  }
+
+  return <InnerInputTask onSubmit={onSubmit} />
 }
