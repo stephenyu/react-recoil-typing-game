734bc866020f3d894617914c0063c874d4bb49ae
stephenyu <stephen@canva.com>
2020-05-17 10:08:00 +1000

    Game

--- src/web/index.tsx
+++ src/web/index.tsx
@@ -1,42 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { useRecoilState, RecoilRoot } from 'recoil';
+import { RecoilRoot } from 'recoil';
 
-import { TasksState } from './atoms/tasks.atom';
-
-import { ListTasks } from './list_tasks/list_tasks';
-import { InputTask } from './input_task/input_task';
-
-const LocationHandler = (): JSX.Element => {
-  const [tasks, setTasks] = useRecoilState(TasksState);
-
-  React.useEffect(() => {
-    const hash = decodeURI(location.hash);
-    if (hash) {
-      const match = hash.match(/#(.*)/);
-      const tasks = match[1].split(',');
-      setTasks(tasks);
-    }
-  }, []);
-
-
-  React.useEffect(() => {
-    if (tasks.length > 0) {
-      const urlData = tasks.join(',');
-      const url = location.origin + location.pathname + `#${urlData}`;
-      location.href = encodeURI(url);
-    }
-  }, [tasks]);
-
-  return null;
-}
+import { Keyboard } from './keyboard/keyboard';
+import { Paragraph } from './paragraph/paragraph';
+import { content } from './animal_farm';
 
 const App = () => {
   return <div>
     <RecoilRoot>
-      <LocationHandler/>
-      <ListTasks/>
-      <InputTask/>
+      <Paragraph content={content}/>
+      <Keyboard/>
     </RecoilRoot>
   </div>
 }
